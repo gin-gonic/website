ed43051d67e777b2e839f11c1f283b0c9fd93d38
wathika-eng <wathika02@gmail.com>
2025-12-13 12:46:37 +0300

    chore: update dependencies and remove unused code
    - Updated @astrojs/sitemap to version 3.3.0
    - Updated @astrojs/starlight to version 0.32.5
    - Downgraded astro to version 5.15.9
    - Downgraded sharp to version 0.32.5
    - Added overrides for mdast-util-to-hast to version 13.2.1
    - Removed commented-out versions collection in content.config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,122 +1,105 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import starlightUtils from "@lorenzo_lewis/starlight-utils";
-import starlightVersions from 'starlight-versions'
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gin-gonic.com',
+  site: "https://gin-gonic.com",
 
-    integrations: [starlight({
-        plugins: [
-            starlightUtils({
-              multiSidebar: {
-                switcherStyle: "horizontalList",
-              },
-            }),
-    
-          
-            // starlightVersions({
-            //   versions: [{ slug: '0.0.1' }],
-            // }),
-          ],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "horizontalList",
+          },
+        }),
+      ],
 
-        title: 'Gin Web Framework',
-        favicon: '/favicon.ico',
-        logo: {
-            src: "./src/assets/gin.png",
-        },
-        
-        defaultLocale: 'en',
-        locales: {
-            'en': {
-                label: 'English',
-                lang: 'en',
-            },
-            'es': {
-                label: 'Español',
-                lang: 'es',
-            },
-            "fa": {
-                label: "Persian",
-                lang: "fa",
-            },
-            "id": {
-                label: "Bahasa Indonesia",
-                lang: "id",
-            },
-            "ja": {
-                label: "日本語",
-                lang: "ja",
-            },
-            "ko-KR": {
-                label: "한국어",
-                lang: "ko-KR",
-            },
-            "pt": {
-                label: "Português",
-                lang: "pt",
-            },
-            "ru": {
-                label: "Russian",
-                lang: "ru",
-            },
-            "tr": {
-                label: "Turkish",
-                lang: "tr",
-            },
-            "zh-CN": {
-                label: "简体中文",
-                lang: "zh-CN",
-            },
-            "zh-TW": {
-                label: "繁體中文",
-                lang: "zh-TW",
-            },
-            "ar": {
-                label: "عربي",
-                lang: "ar",
-            },
-            // "fr": {
-            //     label: "Français",
-            //     lang: "fr",
-            // },
-            // "de": {
-            //     label: "Deutsch",
-            //     lang: "de",
-            // },
-        },
+      title: "Gin Web Framework",
+      favicon: "/favicon.ico",
+      logo: {
+        src: "./src/assets/gin.png",
+      },
 
-        social: [
-            {
-                icon: 'github',
-                href : 'https://github.com/gin-gonic/gin',
-                label: `GitHub`
-            }
-        ],
-        sidebar: [
-            {
-                label: "Docs",
-                autogenerate: { directory: "docs" }
-            },
-            {
-                label: "Blog",
-                autogenerate: { directory: "blog" }
-            },
-        ],
-        expressiveCode: {
-            themes: ['github-dark', 'github-light'],
+      defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+          lang: "en",
         },
-        editLink: {
-            baseUrl: 'https://github.com/gin-gonic/website/edit/master/',
+        es: {
+          label: "Español",
+          lang: "es",
         },
-        customCss: [
-            './src/styles/custom.css',
-        ],
-        lastUpdated: true,
-        credits: false, // Opcional
-    }), sitemap()],
+        fa: {
+          label: "Persian",
+          lang: "fa",
+        },
+        id: {
+          label: "Bahasa Indonesia",
+          lang: "id",
+        },
+        ja: {
+          label: "日本語",
+          lang: "ja",
+        },
+        "ko-kr": {
+          label: "한국어",
+          lang: "ko-kr",
+        },
+        pt: {
+          label: "Português",
+          lang: "pt",
+        },
+        ru: {
+          label: "Russian",
+          lang: "ru",
+        },
+        tr: {
+          label: "Turkish",
+          lang: "tr",
+        },
+        "zh-cn": {
+          label: "简体中文",
+          lang: "zh-cn",
+        },
+        "zh-tw": {
+          label: "繁體中文",
+          lang: "zh-tw",
+        },
+        ar: {
+          label: "عربي",
+          lang: "ar",
+        },
+      },
+
+      social: {
+        github: "https://github.com/gin-gonic/gin",
+      },
+      sidebar: [
+        {
+          label: "Docs",
+          autogenerate: { directory: "docs" },
+        },
+        {
+          label: "Blog",
+          autogenerate: { directory: "blog" },
+        },
+      ],
+      expressiveCode: {
+        themes: ["github-dark", "github-light"],
+      },
+      editLink: {
+        baseUrl: "https://github.com/gin-gonic/website/edit/master/",
+      },
+      customCss: ["./src/styles/custom.css"],
+      lastUpdated: true,
+      credits: false, // Opcional
+    }),
+    sitemap(),
+  ],
 });
